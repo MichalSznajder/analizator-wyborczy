e438235f09f227aa895b6a24b3418a39bd3488cd
Michał Sznajder <michalsznajder@UglyMac.local>
2018-03-05 21:23:00 +0100

    skeleton do downlaod raw districts data

--- districts.py
+++ districts.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import json
+import urllib2
 from pprint import pprint
 from scipy.spatial import ConvexHull
+from bs4 import BeautifulSoup
 
 def get_streets_coord():
     raw_data = json.load(open('only-wro-only-names.geojson'))
@@ -54,6 +56,33 @@ def create_district_points(streets):
         file.write(json_data)
 
 
+def get_points_for_district(district_streets):
+    points = []
+    for street_name in district_streets:
+        if not street_name in streets:
+            raise Exception('Street not found ', street_name)
+
+        for x in streets[street_name]:
+            points.append(x)
+
+    return points    
+
+def get_json_hull_for_points(points):
+    feature = {}
+    feature['type'] = 'Feature'
+    feature['geometry'] = {}    
+    
+    hull = ConvexHull(points)
+    feature['geometry']['type'] = 'Polygon'
+    hull_result = []
+    for v in hull.vertices:
+        hull_result.append(points[v])
+    hull_result.append(hull_result[0])
+    feature['geometry']['coordinates'] = []
+    feature['geometry']['coordinates'].append(hull_result)
+    
+    return feature
+
 def create_district_hulls(streets):
 
     districts_json = {}
@@ -62,27 +91,12 @@ def create_district_hulls(streets):
     districts_json['features'] = []
     districts_features = districts_json['features'];
 
-    hull_points = []
-    for street_name in district_streets:
-        if not street_name in streets:
-            raise Exception('Street not found ', street_name)
+    hull_points = get_points_for_district(district_streets)
+    feature = get_json_hull_for_points(hull_points)
+    districts_features.append(feature)
 
-        for x in streets[street_name]:
-            hull_points.append(x)
-
-    feature = {}
-    feature['type'] = 'Feature'
-    feature['geometry'] = {}    
-    
-    hull = ConvexHull(hull_points)
-    feature['geometry']['type'] = 'Polygon'
-    hull_result = []
-    for v in hull.vertices:
-        hull_result.append(hull_points[v])
-    hull_result.append(hull_result[0])
-    feature['geometry']['coordinates'] = []
-    feature['geometry']['coordinates'].append(hull_result)
-
+    hull_points = get_points_for_district(district_streets2)
+    feature = get_json_hull_for_points(hull_points)
     districts_features.append(feature)
 
     json_data = json.dumps(districts_json)
@@ -107,8 +121,44 @@ district_streets = [
     u'Widok', 
     u'Wierzbowa']
 
-streets = get_streets_coord()
-create_district_points(streets)
-create_district_hulls(streets)
+district_streets2 = [
+    u"Biskupia", 
+    u"Kazimierza Wielkiego",
+    u"Krawiecka", 
+    u"Kurzy Targ", 
+    u"Kuźnicza", 
+    u"Łaciarska",
+    u"Ofiar Oświęcimskich",
+    u"Oławska",
+    u"Plac Świętego Krzysztofa",
+    u"Szewska", 
+    u"Świdnicka",
+    u"Wita Stwosza"]
+
+# streets = get_streets_coord()
+# create_district_points(streets)
+# create_district_hulls(streets)
 
 
+
+class RawDistrictData(object):
+    streets = ""
+
+def get_raw_districts_list():
+    response = urllib2.urlopen('http://wybory2011.pkw.gov.pl/geo/020000/pl/026401.html')
+    html = response.read()
+    soup = BeautifulSoup(html, 'lxml')
+    tbody = soup.html.body.div.contents[5].contents[3].table.tbody
+
+    districts = []
+    for row in tbody.find_all('tr')[2:]:
+        raw_data = RawDistrictData()
+        cells = row.find_all('td')
+        raw_data.streets = cells[5].string
+        districts.append(raw_data)
+       
+    return districts
+
+raw_distrcits = get_raw_districts_list()
+
+pprint(raw_distrcits)
